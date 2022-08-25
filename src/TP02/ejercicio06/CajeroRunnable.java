0bc2195bbf2f9ec2b849d608a3683a48da8a7242
Desarrollo Contaduria <81999055+GiacoboniConstanza@users.noreply.github.com>
2022-08-25 10:30:19 -0300

    ejercicio 07

---
+++ src/TP02/ejercicio06/CajeroRunnable.java
@@ -0,0 +1,48 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Other/File.java to edit this template
+ */
+
+/**
+ *
+ * @author 23437650624
+ */
+public class CajeroRunnable implements Runnable {
+
+    private String nombre;
+    private Cliente cliente;
+    private long initialTime;
+
+    public CajeroRunnable(String nombre, Cliente cliente, long initialTIme) {
+        this.nombre = nombre;
+        this.cliente = cliente;
+        this.initialTime = initialTime;
+
+    }
+
+    public void run() {
+        System.out.println("El cajero " + this.nombre
+                + " COMIENZA A PROCESAR LA COMPRA DEL CLIENTE "
+                + this.cliente.getNombre() + " EN EL TIEMPO: "
+                + (System.currentTimeMillis() - this.initialTime) / 1000 + "seg");
+        for (int i = 0; i < this.cliente.getCarroCompra().length; i++) {
+            this.esperarXsegundos(cliente.getCarroCompra()[i]);
+            System.out.println("Procesado el producto " + (i + 1) + " del cliente "
+                    + this.cliente.getNombre() + "->Tiempo: "
+                    + (System.currentTimeMillis() - this.initialTime) / 1000 + "seg");
+        }
+        System.out.println("El cajero" + this.nombre + " HA TERMINADO DE PROCESAR "
+                + this.cliente.getNombre() + " EN EL TIEMPO: "
+                + (System.currentTimeMillis() - this.initialTime) / 1000
+                + "seg");
+    }
+
+    private void esperarXsegundos(int tiempo) {
+        tiempo = tiempo * 1000;
+        try {
+            Thread.sleep(tiempo);
+        } catch (InterruptedException e) {
+            System.out.println(this.nombre + " interrumpido");
+        }
+    }
+}
---
+++ src/TP02/ejercicio06/MainRunnable.java
@@ -0,0 +1,27 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Other/File.java to edit this template
+ */
+
+/**
+ *
+ * @author 23437650624
+ */
+public class MainRunnable {
+
+    public static void main(String args[]) {
+        Cliente cliente1 = new Cliente("Cliente 1", new int[]{2, 2, 1, 5, 2, 3});
+        Cliente cliente2 = new Cliente("Cliente 2", new int[]{1, 3, 5, 1, 1});
+
+        long initialTime = System.currentTimeMillis();
+
+        CajeroRunnable cajero1 = new CajeroRunnable("Cajero 1", cliente1, initialTime);
+        CajeroRunnable cajero2 = new CajeroRunnable("Cajero 2", cliente2, initialTime);
+
+        Thread c1 = new Thread(cajero1);
+        Thread c2 = new Thread(cajero2);
+
+        c1.start();
+        c2.start();
+    }
+}
